37ff96f2890f5090ae08c34cf785509d3ca9de4d
Ahmed Osama <155636766+AhmdBlack0@users.noreply.github.com>
2024-09-25 18:11:08 +0300

    Add files via upload

---
+++ Prayer_Times/main.js
@@ -0,0 +1,43 @@
+function ci() {
+  let selectedCity = document.querySelector("select").value;
+  return selectedCity ? selectedCity : "Cairo"; // Default to Cairo if no city is selected
+}
+
+function fetchPrayerTimes() {
+  axios
+    .get("http://api.aladhan.com/v1/timingsByCity", {
+      params: {
+        country: "EG",
+        city: ci(),
+      },
+    })
+    .then(function (response) {
+      console.log(response.data.data);
+      document.querySelector(".fajr .time").innerHTML =
+        response.data.data.timings.Fajr;
+      document.querySelector(".sunrise .time").innerHTML =
+        response.data.data.timings.Sunrise;
+      document.querySelector(".dhuhr .time").innerHTML =
+        response.data.data.timings.Dhuhr;
+      document.querySelector(".asr .time").innerHTML =
+        response.data.data.timings.Asr;
+      document.querySelector(".maghrib .time").innerHTML =
+        response.data.data.timings.Maghrib;
+      document.querySelector(".isha .time").innerHTML =
+        response.data.data.timings.Isha;
+        document.querySelector(".date").innerHTML =
+          response.data.data.date.gregorian.date;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+// Event listener to fetch prayer times when a city is selected
+let cities = document.querySelectorAll("select");
+cities.forEach((city) => {
+  city.addEventListener("change", fetchPrayerTimes);
+});
+
+// Fetch prayer times for the default city (Cairo) on page load
+fetchPrayerTimes();
